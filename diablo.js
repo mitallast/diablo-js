b1f10bbb1e8cf66ced5a9ad4fbdb5179d8fedd29
Alexey Korchevsky <mitallast@gmail.com>
2012-06-25 22:04:40 +0400

    now houses renders as real object

--- diablo.js
+++ diablo.js
@@ -6,8 +6,8 @@ var tw=512, th=tw/2, s=tw*0.705, a=Math.PI/4, log=[];
 var floorMap = [
 	"00000000000000 000000000000000000000000000000000".split(""),
 	"01111000000000 000000000000000000000000000000000".split(""),
-	"00021000000000 000000000000000000000000000000000".split(""),
-	"000211111111111111110000000000000000000000000000".split(""),
+	"00221000000000 000000000000000000000000000000000".split(""),
+	"000111111111111111110000000000000000000000000000".split(""),
 	"00000000100000 001000000000000000000000000000000".split(""),
 	"00000000100000 001111111100000000000000000000000".split(""),
 	"00000000100000 000000000011111100000000000000000".split(""),
@@ -15,11 +15,11 @@ var floorMap = [
 	"000000001100000000000000000000000000000000000000".split(""),
 	"000000001111111000000000000000000000000000000000".split(""),
 	"000000000100011000000000000000000000000000000000".split(""),
-	"000000000100002000000000000000000000000000000000".split(""),
+	"000000000100001000000000000000000000000000000000".split(""),
 	"000000000100000000000000000000000000000000000000".split(""),
 	"000000000100000000000000000000000000000000000000".split(""),
 	"000000000110000000000000000000000000000000000000".split(""),
-	"000000211111110000000000000000000000000000000000".split(""),
+	"000000111111110000000000000000000000000000000000".split(""),
 	"000000000000000000000000000000000000000000000000".split(""),
 	"000000000000000000000000000000000000000000000000".split(""),
 	"000000000000000000000000000000000000000000000000".split(""),
@@ -34,26 +34,33 @@ var floorMap = [
 var floor = document.getElementById("floor").getContext("2d");
 floor.w = floor.canvas.width;
 floor.h = floor.canvas.height;
-floor.click_x=0;
-floor.click_y=0;
 
 var tileMap = {
 	0: getImage("texture/tileable-grey.png"),
 	1: getImage("texture/dirtsand.png"),
 	2: getImage("texture/tileable-grey.png"),
 };
-var spriteMap = {
-	2: getImage("sprite/house.png"),
-};
 
-var hero = new person("king_artur");
-var monsters = [];
-for(var i=0;i<33;i++){
-	var p = new person("safria_elf");
-	p.to_x = p.x = randomx();
-	p.to_y = p.y = randomy();
-	monsters.push(p);
+// pre-fetch textures for Person class
+var personTextures={};
+var personTypes=["king_artur","safria_elf"];
+for(var pt in personTypes){
+	var name = personTypes[pt];
+	var p = {stand:[],anim:{}};
+	for(var i=1;i<=8;i++){
+		p.stand.push(getImage("char/"+name+"/stand/"+i+".gif"));
+		p.anim[i-1]=[];
+		for(var j=1;j<=15;j++){
+			var j_s = j<10?"0"+j:j;
+			p.anim[i-1].push(getImage("char/"+name+"/anim/"+i+"_"+j_s+".gif"));
+		}
+	}
+	personTextures[name] = p;
 }
+
+var hero = new Person("king_artur",s*1.5,s*1.5);
+var monsters = [];
+for(var i=0;i<33;i++) monsters.push(new Person("safria_elf",randomx(),randomy()));
 
 setInterval(function() {
 	var m = monsters[Math.ceil(Math.random()*(monsters.length-1))];
@@ -62,32 +69,58 @@ setInterval(function() {
 }, 200);
 
 var barrels=[];
-for(var i=0;i<33;i++){
-	var b = new barrel();
-	b.x = randomx();
-	b.y = randomy();
-	barrels.push(b);
-}
+for(var i=0;i<33;i++) barrels.push(new Barrel(randomx(),randomy()));
+
 var coins=[];
+var houses=[];
+for(var y in floorMap){ // pre fetch house sprites;
+	for(var x in floorMap[y]){
+		if(floorMap[y][x]=="2"){
+			var h = new House();
+			h.x = (parseInt(x)+0.5)*s;  // at center of texture
+			h.y = (parseInt(y)+0.5)*s;
+			houses.push(h);
+		}
+	}
+}
+
 var barrelSprite = getImage("sprite/barrel64.png");
 var coinSprite = getImage("sprite/coins10.png");
-function barrel(){
+var houseSprite = getImage("sprite/house.png");
+
+function House(){
 	this.x=0;
 	this.y=0;
+	this.offset_y=th/2; // render offset
+	this.sprite=function(){
+		return houseSprite;
+	};
+	this.isOverHero = function(){
+		var hx = (hero.x - hero.y) * Math.cos(a),
+			hy = (hero.x + hero.y)/2 * Math.sin(a);
+		var sx = (this.x - this.y) * Math.cos(a),
+			sy = (this.x + this.y)/2 * Math.sin(a);
+		return (hx >= sx-houseSprite.width/2)
+			&& (hx <= sx+houseSprite.width/2)
+			&& (hy >= sy+this.offset_y-houseSprite.height)
+			&& (hy <= sy)
+	}
+}
+function Barrel(x, y){
+	this.x=x;
+	this.y=y;
 	this.sprite=function(){
 		return barrelSprite;
 	};
 	this.click=function(){
 		remove(barrels,this);
-		var c = new coin();
-		c.x = this.x;
-		c.y = this.y;
+		var c = new Coin(this.x, this.y);
 		coins.push(c);
 	};
 }
-function coin(){
-	this.x=0;
-	this.y=0;
+function Coin(x,y){
+	this.x=x;
+	this.y=y;
 	this.coins=Math.floor(Math.random()*100);
 	this.sprite=function(){
 		return coinSprite;
@@ -121,13 +154,19 @@ function renderObjects(){
 	for(var m in monsters)zb.push(monsters[m]);
 	for(var b in barrels)zb.push(barrels[b]);
 	for(var c in coins)zb.push(coins[c]);
+	for(var h in houses)zb.push(houses[h]);
 	zb.sort(function(b,a){ return b.x+b.y-a.x-a.y; });//first is desc
 	for(z in zb){
 		var m = zb[z];
-		var mrx = m.x * Math.cos(a) - m.y * Math.sin(a),
-			mry = (m.x * Math.sin(a) + m.y * Math.cos(a))/2;
+		var sx = (m.x - m.y)*Math.cos(a),
+			sy = (m.x + m.y)/2*Math.sin(a); // simplified rotate
+		if(m.offset_y) sy+=m.offset_y;
 		var tile = m.sprite();
-		floor.drawImage(tile, Math.round(mrx-tile.width/2), Math.round(mry-tile.height));
+		if(m.isOverHero && m.isOverHero()){
+			floor.globalAlpha = 0.5;
+		} 
+		floor.drawImage(tile, Math.round(sx-tile.width/2), Math.round(sy-tile.height));
+		floor.globalAlpha = 1;
 	}
 	floor.restore();
 }
@@ -151,23 +190,6 @@ function renderFloor() {
 			var tx = ( x - y ) * th, // for normal sprite
 				ty = ( x + y ) * th/2;
 			floor.drawImage(tile, tx, ty, tw, th);
-			var spr = spriteMap[f];
-			if(spr){
-				var sx = tx, // for non-normal height sprite
-					sy = ty - (spr.height - th);
-				// if hero over ( th is for hero)
-				if( mrx > sx-th && mrx < sx+spr.width-th
-					&& mry > sy && mry < sy+spr.height-th/2
-				){  // draw as transparent sprite
-					floor.save();
-					floor.globalAlpha = 0.5;
-					// @todo scale to s constant
-					floor.drawImage(spr, sx, sy);
-					floor.restore();
-				}else{
-					floor.drawImage(spr, sx, sy);
-				}
-			}
 		}
 	}
 	floor.translate(th, 0); // retranslate for diamond textures
@@ -176,8 +198,8 @@ function renderFloor() {
 }
 
 function remove(ar,v){var i=ar.indexOf(v);if(i>=0)ar.splice(i,1);}
-function randomx(){return Math.floor(Math.random()*(floorMap[0].length-1)*s);}
-function randomy(){return Math.floor(Math.random()*(floorMap.length-1)*s);}
+function randomx(){return Math.floor(Math.random()*(floorMap[0].length/4)*s);}
+function randomy(){return Math.floor(Math.random()*(floorMap.length/4)*s);}
 
 function typeByPoint(x,y){
 	var dx=Math.floor(x/s), 
@@ -188,26 +210,24 @@ function typeByPoint(x,y){
 function renderLog(){
 	floor.save();
 	floor.fillStyle='#fff';
-	floor.font = '10px Arial'
-	for(var i in log)
-		floor.fillText(log[i],10,20+(20*i),floor.width-20);
+	floor.font = '10px Arial';
+	for(var i in log) floor.fillText(log[i],10,20+(20*i),floor.width-20);
 	floor.restore();
 }
 
 floor.canvas.onclick = function(e) { 
 	// untranslate
-	var mx = e.offsetX - floor.w/2,
-		my = e.offsetY - floor.h/2;
-	my *= 2; //unscale
+	var mx = e.offsetX - floor.w/2;
+	var my = e.offsetY - floor.h/2;
+		my *= 2; //unscale
 	// unrotate
-	var a = - Math.PI / 4;
-	floor.click_x = hero.x + mx * Math.cos(a) - my * Math.sin(a);
-	floor.click_y = hero.y + mx * Math.sin(a) + my * Math.cos(a);
+	floor.click_x = hero.x + mx * Math.cos(-a) - my * Math.sin(-a);
+	floor.click_y = hero.y + mx * Math.sin(-a) + my * Math.cos(-a);
+	
 	processClick();
-	// translate
+	
 	hero.to_x = floor.click_x;
 	hero.to_y = floor.click_y;
-	return false;
 };
 
 setInterval(function() {
@@ -218,26 +238,18 @@ setInterval(function() {
 }, 33);
 
 
-function person(name){
-	this.stand=[];
-	this.anim={};
-	this.x=0;
-	this.y=0;
+function Person(name,x,y){
+	this.name=name;
+	this.stand=personTextures[this.name].stand;
+	this.anim=personTextures[this.name].anim;
+	this.x=x;
+	this.y=y;
 	this.to_x = this.x;
 	this.to_y = this.y;
-	this.a = 4; // from 0 to 7, where 0 is at 12 o´clock
+	this.a = 4; // angel, from 0 to 7, where 0 is at 12 o´clock
 	this.walk = false;
 	this.step = 0;
 	this.coins=0;
-	// init textures;
-	for(var i=1;i<=8;i++){
-		this.stand.push(getImage("char/"+name+"/stand/"+i+".gif"));
-		this.anim[i-1]=[];
-		for(var j=1;j<=15;j++){
-			var j_s = j<10?"0"+j:j;
-			this.anim[i-1].push(getImage("char/"+name+"/anim/"+i+"_"+j_s+".gif"));
-		}
-	}
 	this.nextStep = function(){
 		this.walk=false;
 		var sx = dx = (this.to_x - this.x),
@@ -254,7 +266,7 @@ function person(name){
 		var t = typeByPoint(x,y);
 		if(typeof t == 'undefined') return; // if on not map
 		if(t == " ") return; // if empty slot
-		if(spriteMap[t]) return; // if on sprite
+		if(t == "2") return; // if House
 		this.x=x;
 		this.y=y;
 		this.a = Math.round((Math.atan2(sy, sx)/Math.PI+2.75)%2*4)%8;
