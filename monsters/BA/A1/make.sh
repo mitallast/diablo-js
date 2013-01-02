6ede3ce5fe10b6a87e9fd4569737d33759cc1230
Alexey Korchevsky <mitallast@gmail.com>
2013-01-02 14:17:39 +0400

    move barbarian

---
+++ monsters/BA/A1/make.sh
@@ -0,0 +1,18 @@
+a=0 && convert +append $a-0.png $a-2.png $a-4.png $a-6.png $a-8.png $a-10.png $a-12.png $a-14.png $a-16.png $a-17.png $a.png
+a=1 && convert +append $a-0.png $a-2.png $a-4.png $a-6.png $a-8.png $a-10.png $a-12.png $a-14.png $a-16.png $a-17.png $a.png
+a=2 && convert +append $a-0.png $a-2.png $a-4.png $a-6.png $a-8.png $a-10.png $a-12.png $a-14.png $a-16.png $a-17.png $a.png
+a=3 && convert +append $a-0.png $a-2.png $a-4.png $a-6.png $a-8.png $a-10.png $a-12.png $a-14.png $a-16.png $a-17.png $a.png
+a=4 && convert +append $a-0.png $a-2.png $a-4.png $a-6.png $a-8.png $a-10.png $a-12.png $a-14.png $a-16.png $a-17.png $a.png
+a=5 && convert +append $a-0.png $a-2.png $a-4.png $a-6.png $a-8.png $a-10.png $a-12.png $a-14.png $a-16.png $a-17.png $a.png
+a=6 && convert +append $a-0.png $a-2.png $a-4.png $a-6.png $a-8.png $a-10.png $a-12.png $a-14.png $a-16.png $a-17.png $a.png
+a=7 && convert +append $a-0.png $a-2.png $a-4.png $a-6.png $a-8.png $a-10.png $a-12.png $a-14.png $a-16.png $a-17.png $a.png
+a=8 && convert +append $a-0.png $a-2.png $a-4.png $a-6.png $a-8.png $a-10.png $a-12.png $a-14.png $a-16.png $a-17.png $a.png
+a=9 && convert +append $a-0.png $a-2.png $a-4.png $a-6.png $a-8.png $a-10.png $a-12.png $a-14.png $a-16.png $a-17.png $a.png
+a=10 && convert +append $a-0.png $a-2.png $a-4.png $a-6.png $a-8.png $a-10.png $a-12.png $a-14.png $a-16.png $a-17.png $a.png
+a=11 && convert +append $a-0.png $a-2.png $a-4.png $a-6.png $a-8.png $a-10.png $a-12.png $a-14.png $a-16.png $a-17.png $a.png
+a=12 && convert +append $a-0.png $a-2.png $a-4.png $a-6.png $a-8.png $a-10.png $a-12.png $a-14.png $a-16.png $a-17.png $a.png
+a=13 && convert +append $a-0.png $a-2.png $a-4.png $a-6.png $a-8.png $a-10.png $a-12.png $a-14.png $a-16.png $a-17.png $a.png
+a=14 && convert +append $a-0.png $a-2.png $a-4.png $a-6.png $a-8.png $a-10.png $a-12.png $a-14.png $a-16.png $a-17.png $a.png
+a=15 && convert +append $a-0.png $a-2.png $a-4.png $a-6.png $a-8.png $a-10.png $a-12.png $a-14.png $a-16.png $a-17.png $a.png
+convert -append 0.png 1.png 2.png 3.png 4.png 5.png 6.png 7.png 8.png 9.png 10.png 11.png 12.png 13.png 14.png 15.png map.png
+rm 0.png 1.png 2.png 3.png 4.png 5.png 6.png 7.png 8.png 9.png 10.png 11.png 12.png 13.png 14.png 15.png
